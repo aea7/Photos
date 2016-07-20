9e671e422a448cd88f7459b464ab3d5226ce9919
Abdullah Ak <aak@ku.edu.tr>
2016-07-20 11:16:43 +0300

    Images upload, index ok

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -37,7 +37,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   version :index do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fit => [600, 600]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
